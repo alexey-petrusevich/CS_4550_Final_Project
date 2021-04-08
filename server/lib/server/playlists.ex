e3a64acebe2f37a2e87d40821e205ae4dded5857
Alexey Petrusevich <apetrusevich1@gmail.com>
2021-04-07 22:35:04 -0400

    Updated song info retrieval from spotify

--- server/lib/server/playlists.ex
+++ server/lib/server/playlists.ex
@@ -51,60 +51,140 @@ defmodule Server.Playlists do
     tracks = get_playlist_tracks(playlist_uri, token.token)
     # 2) add it to the db
     # store tracks in the db
-    store_tracks(tracks, party_id)
+    store_tracks(tracks, party_id, token)
   end
 
+
   # returns a list of tracks given playlist_uri (and token)
-  # title, artist, genre???, uri of each track
+  # each track represents a map entry containing the following data:
+  # - artist
+  # - title
+  # - track_uri
+  # - genre
+  # - energy
+  # - danceability
+  # - loudness
+  # - valence
+  # tested
   def get_playlist_tracks(playlist_uri, token) do
     uri = playlist_uri
-    |> String.split(":")
-    |> Enum.at(2)
-
-
+          |> String.split(":")
+          |> Enum.at(2)
     url = "https://api.spotify.com/v1/playlists/#{uri}/tracks"
-    headers = headers = [
+    headers = [
       "Accept": "application/json",
       "Content-Type": "application/json",
       "Authorization": "Bearer #{token}}",
     ]
     resp = HTTPoison.get!(url, headers)
     data = Jason.decode!(resp.body)
-
     tracks = Enum.map(
       data["items"],
       fn item ->
         title = item["track"]["name"]
         track_uri = item["track"]["uri"]
         # assumption that there is a single artist for every track
-        artist = item["track"]["artists"] |> Enum.at(0) |> Map.get("name")
-        artist_uri = item["track"]["artists"] |> Enum.at(0) |> Map.get("uri")
+        artist = item["track"]["artists"]
+                 |> Enum.at(0)
+                 |> Map.get("name")
+        artist_uri = item["track"]["artists"]
+                     |> Enum.at(0)
+                     |> Map.get("uri")
 
-        genre = "Hip Hop"; # TODO update after artist API call
-        # idk why item["track]["artists"][0] isnt working anymore
-
-        # TODO alex : call the two new apis (get artist genre and get track data) and update
-        #             track fields to be written to the database rather than defaults I hardcoded below
-        %{title: title, track_uri: track_uri, artist: artist, genre: genre}
+        genre = get_track_genre(artist_uri, token);
+        {energy, danceability, loudness, valence} = get_track_stats(track_uri, token)
+        %{
+          artist: artist,
+          title: title,
+          track_uri: track_uri,
+          genre: genre,
+          energy: energy,
+          danceability: danceability,
+          loudness: loudness,
+          valence: valence
+        }
       end
     )
     tracks
   end
 
-  #given a collection of tracks, stores each song in the DB
-  def store_tracks(tracks, party_id) do
-   Enum.map(
-     tracks,
-     fn track ->
-       genre = "" # TODO: resolve having no genre in JSON response from spotify
-       Server.Songs.create_song(
-         %{artist: track.artist,
-              title: track.title, track_uri: track.track_uri,
-              party_id: party_id, played: false, genre: track.genre,
-              energy: "0", danceability: "0", loudness: "0",
-              valence: ""}
-       )
-     end
-   )
+
+  # given a collection of tracks, stores each song in the DB
+  def store_tracks(tracks, party_id, token) do
+    Enum.map(
+      tracks,
+      fn track ->
+        Server.Songs.create_song(
+          %{
+            artist: track.artist,
+            title: track.title,
+            track_uri: track.track_uri,
+            party_id: party_id,
+            played: false,
+            genre: track.genre,
+            energy: track.energy,
+            danceability: track.danceability,
+            loudness: track.loudness,
+            valence: track.valence
+          }
+        )
+      end
+    )
   end
+
+
+  # return track genre given track_artist and spotify access token
+  # in case there are more than one genres, return the first return by API
+  # if no genres returned by API call, returns "none" for genre
+  def get_track_genre(track_artist, token) do
+    track_artist = get_artists_id(track_artist)
+    url = "https://api.spotify.com/v1/artists/#{track_artist}"
+    headers = [
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": "Bearer #{token}}",
+    ]
+    resp = HTTPoison.get!(url, headers)
+    data = Jason.decode!(resp.body)
+    # manual delay because requesting too fast fails
+    Process.sleep(100)
+    genres = data["genres"]
+    if (length(genres) == 0) do
+      "none"
+    else
+      hd(data["genres"])
+    end
+  end
+
+
+  # returns a tuple containing the following data given track_uri and access token:
+  # {energy, danceability, loudness, valence}
+  def get_track_stats(track_uri, token) do
+    track_id = get_track_id(track_uri)
+    url = "https://api.spotify.com/v1/audio-features/#{track_id}"
+    headers = [
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": "Bearer #{token}}",
+    ]
+    resp = HTTPoison.get!(url, headers)
+    data = Jason.decode!(resp.body)
+    {data["energy"], data["danceability"], data["loudness"], data["valence"]}
+  end
+
+
+  # returns track id given track uri
+  def get_track_id(track_uri) do
+    track_uri
+    |> String.split(":")
+    |> Enum.at(2)
+  end
+
+  def get_artists_id(artists_uri) do
+    artists_uri
+    |> String.split(":")
+    |> Enum.at(2)
+  end
+
+
 end
