93cec321d1bcd6d5f3cb327e50fd7ccaf569f32a
Alexey Petrusevich <apetrusevich1@gmail.com>
2021-04-08 01:06:36 -0400

    COmmented vote controller

--- server/lib/server_web/controllers/vote_controller.ex
+++ server/lib/server_web/controllers/vote_controller.ex
@@ -9,27 +9,27 @@ defmodule ServerWeb.VoteController do
 
   # updates (creates) a vote for the given song
   def vote(conn, data) do
-    # user_id, song_id, value
-    user_id = data["user_id"]
-    song_id = data["song_id"]
-    value = data["value"]
-    # check if entry already exists
-    {:ok, vote} = Votes.get_vote_by_song_id!(song_id)
-    if (vote) do
-      vote.value = vote.value + value
-      Votes.update_vote(vote)
-      conn
-      |> send_resp(:no_content, "")
-    else
-      newVote = %{
-        user_id: user_id,
-        song_id: song_id,
-        value: value
-      }
-      Votes.create_vote(newVote)
-      conn
-      |> send_resp(:no_content, "")
-    end
+#    # user_id, song_id, value
+#    user_id = data["user_id"]
+#    song_id = data["song_id"]
+#    value = data["value"]
+#    # check if entry already exists
+#    vote = Votes.get_vote_by_song_id!(song_id)
+#    if (vote) do
+#      vote["value"] = vote["value"] + value
+#      Votes.update_vote(vote)
+#      conn
+#      |> send_resp(:no_content, "")
+#    else
+#      newVote = %{
+#        user_id: user_id,
+#        song_id: song_id,
+#        value: value
+#      }
+#      Votes.create_vote(newVote)
+#      conn
+#      |> send_resp(:no_content, "")
+#    end
   end
 
 end
