4361ccf086c4dc8b583b5ca92b379c49af593dc6
morrisonineu <morrison.i@northeastern.edu>
2021-04-08 11:46:55 -0400

    First commit

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+export MIX_ENV=prod
+export PORT=4780
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+
+echo "Building..."
+
+mix deps.get
+mix compile
+(cd assets && npm install)
+(cd assets && webpack --mode production)
+mix phx.digest
+
+echo "Generating release..."
+mix release
+
+#echo "Stopping old copy of app, if any..."
+#_build/prod/rel/practice/bin/practice stop || true
+
+echo "Starting app..."
+
+PROD=t ./start.sh
---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+export MIX_ENV=prod
+export PORT=4801
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/practice/bin/practice stop || true
+
+echo "Starting app..."
+
+_build/prod/rel/practice/bin/practice start
