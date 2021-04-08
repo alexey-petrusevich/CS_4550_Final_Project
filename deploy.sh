07a2dbd335db60db43c7e7f33ddc5e835883aea6
morrisonineu <morrison.i@northeastern.edu>
2021-04-08 18:53:41 -0400

    Made some changes to deploy and start scripts

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+# export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
-export PORT=4780
-export NODEBIN=`pwd`/assets/node_modules/.bin
-export PATH="$PATH:$NODEBIN"
+export PORT=4801
+# export NODEBIN=`pwd`/assets/node_modules/.bin
+# export PATH="$PATH:$NODEBIN"
 
 echo "Building..."
 
-mix deps.get
+mix deps.get --only prod
 mix compile
-(cd assets && npm install)
-(cd assets && webpack --mode production)
-mix phx.digest
 
+#(cd assets && npm install)
+#(cd assets && webpack --mode production)
+#mix phx.digest
+
+mix ecto.reset
 echo "Generating release..."
 mix release
 
 #echo "Stopping old copy of app, if any..."
 #_build/prod/rel/practice/bin/practice stop || true
 
-echo "Starting app..."
-
-PROD=t ./start.sh
+echo "Deploy successful..."
+echo "Run start.sh to start your server"
