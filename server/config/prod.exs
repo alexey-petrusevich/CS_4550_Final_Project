c4a419f34d872b82e5ed4e86169ff8217ac1bc36
Alexey Petrusevich <apetrusevich1@gmail.com>
2021-04-10 13:50:26 -0400

    Updated config

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -14,8 +14,8 @@ config :server,
        url: [
          host: "spotifyparty.quickjohnny.art",
          port: 80
-       ],
-       cache_static_manifest: "priv/static/cache_manifest.json"
+       ]
+#       cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
