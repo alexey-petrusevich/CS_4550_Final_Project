d5882a028eaf70f5ebf8ce50c3f7de942fa3aca5
benockert <55101856+benockert@users.noreply.github.com>
2021-04-10 19:42:25 -0400

    Updated redirect uri back to front end from server

--- server/lib/server_web/controllers/auth_controller.ex
+++ server/lib/server_web/controllers/auth_controller.ex
@@ -16,31 +16,20 @@ defmodule ServerWeb.AuthController do
     |> Map.put("token", token)
     |> Map.put("user_id", user_id |> String.to_integer())
     AuthTokens.create_auth_token(auth_params)
-
-    redirect_uri = cond do
-      System.get_env("MIX_ENV") == "prod" -> System.get_env("REACT_APP_PROD_URL")
-      true -> System.get_env("REACT_APP_DEV_CLIENT_URL")
-    end
-    IO.inspect(redirect_uri)
-
+    
     # redirect back to the front-end to complete the flo
     conn
     |> put_resp_header("content-type", "application/json; charset=UTF-8")
-    |> redirect(external: "http://" <> redirect_uri <> "/?code=hidden")
+    |> redirect(external: "http://spotifyparty.benockert.site/?code=hidden")
   end
 
   # error: Access Denied
   def callback(conn, %{"error" => error}) do
-    redirect_uri = cond do
-      System.get_env("MIX_ENV") == "prod" -> System.get_env("REACT_APP_PROD_URL")
-      true -> System.get_env("REACT_APP_DEV_CLIENT_URL")
-    end
-    IO.inspect(redirect_uri)
 
     # redirect back to the front-end to complete the flow
     conn
     |> put_resp_header("content-type", "application/json; charset=UTF-8")
-    |> redirect(external: "http://" <> redirect_uri <> "/?error=#{error}")
+    |> redirect(external: "http://spotifyparty.benockert.site/?error=#{error}")
   end
 
 end
