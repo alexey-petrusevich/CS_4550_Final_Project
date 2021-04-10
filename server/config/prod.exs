b970ee83745b6d7600fb4f54ebc90bc3bdf0567a
benockert <55101856+benockert@users.noreply.github.com>
2021-04-10 19:38:51 -0400

    Updated prod.exs with benockert.site

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -12,10 +12,9 @@ use Mix.Config
 config :server,
        ServerWeb.Endpoint,
        url: [
-         host: "spotifyparty.quickjohnny.art",
+         host: "spotifyparty.benockert.site",
          port: 80
        ]
-#       cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
