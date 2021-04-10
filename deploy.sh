5aad9fefc06aaa29e64a0a53868cabe689ee18e8
Iain B. Morrison <71100718+morrisonineu@users.noreply.github.com>
2021-04-10 18:31:05 -0400

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+export MIX_ENV=prod
+export PORT=4810
+export DATABASE_URL=ecto://spotify:Sp0t!fY123@localhost/spotifyparty_dev # TODO: Is this localhost or host URL?
+# export NODEBIN=`pwd`/assets/node_modules/.bin
+# export PATH="$PATH:$NODEBIN"
+
+echo "Building..."
+
+mix deps.get --only prod
+mix compile
+
+#(cd assets && npm install)
+#(cd assets && webpack --mode production)
+#mix phx.digest
+
+mix ecto.reset
+echo "Generating release..."
+mix release
+
+#echo "Stopping old copy of app, if any..."
+#_build/prod/rel/practice/bin/practice stop || true
+
+echo "Deploy successful..."
+echo "Run start.sh to start your server"
