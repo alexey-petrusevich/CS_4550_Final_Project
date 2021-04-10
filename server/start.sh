b6f8f8b74b1d93a4c86c0a687c09d38dd5eba22a
morrisonineu <morrison.i@northeastern.edu>
2021-04-10 16:24:04 -0400

    Added branch and deploy scripts

--- server/start.sh
+++ server/start.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
+export SECRET_KEY_BASE
 export MIX_ENV=prod
 export PORT=4810
 
-CFGD=$(readlink -f ~/.config/server)
+echo "Stopping old copy of app, if any..."
 
-if [ ! -e "$CFGD/base" ]; then
-  echo "run deploy first"
-  exit 1
-fi
+# Replace 'practice' with proper app name
+_build/prod/rel/server/bin/server stop || true
 
-DB_PASS=$(cat "$CFGD/db_pass")
-export DATABASE_URL=ecto://spotifyparty:$DB_PASS@localhost/spotifyparty_prod
-export REACT_APP_PROD_URL=spotifyparty.quickjohnny.art
+echo "Starting app..."
 
-SECRET_KEY_BASE=$(cat "$CFGD/base")
-export SECRET_KEY_BASE
-
-_build/prod/rel/server/bin/server start
+# Replace 'practice' with proper app name
+_build/prod/rel/server/bin/server start                                
